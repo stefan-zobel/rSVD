d468fed6b8e3df517325c3f2000a3dc8fd94c288
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-08-06 00:07:58 +0200

    fix whitespace

--- src/main/java/math/rsvd/ApproximateBasis.java
+++ src/main/java/math/rsvd/ApproximateBasis.java
@@ -21,104 +21,104 @@ import net.jamu.matrix.SvdD;
 
 public final class ApproximateBasis {
 
-	// Oversampling parameter
-	private static final int P = 5;
+    // Oversampling parameter
+    private static final int P = 5;
 
-	private final MatrixD A;
-	private final int m;
-	private final int n;
-	private final int targetRank;
-	private final boolean transpose;
+    private final MatrixD A;
+    private final int m;
+    private final int n;
+    private final int targetRank;
+    private final boolean transpose;
 
-	public ApproximateBasis(MatrixD A, int estimatedRank) {
-		if (estimatedRank < 0) {
-			throw new IllegalArgumentException("estimatedRank: " + estimatedRank);
-		}
-		m = A.numRows();
-		n = A.numColumns();
-		transpose = (m < n) ? true : false;
-		this.A = A;
-		targetRank = estimatedRank;
-	}
+    public ApproximateBasis(MatrixD A, int estimatedRank) {
+        if (estimatedRank < 0) {
+            throw new IllegalArgumentException("estimatedRank: " + estimatedRank);
+        }
+        m = A.numRows();
+        n = A.numColumns();
+        transpose = (m < n) ? true : false;
+        this.A = A;
+        targetRank = estimatedRank;
+    }
 
-	public SVD computeSVD() {
-		MatrixD[] BQ = computeBQ();
-		MatrixD B = BQ[0];
-		MatrixD Q = BQ[1];
+    public SVD computeSVD() {
+        MatrixD[] BQ = computeBQ();
+        MatrixD B = BQ[0];
+        MatrixD Q = BQ[1];
 
-		SvdD svd = B.svdEcon();
-		MatrixD U_tilde = svd.getU();
-		double[] sigma = svd.getS();
-		MatrixD Vt = svd.getVt();
+        SvdD svd = B.svdEcon();
+        MatrixD U_tilde = svd.getU();
+        double[] sigma = svd.getS();
+        MatrixD Vt = svd.getVt();
 
-		MatrixD U = null;
-		if (transpose) {
-			U = Q.times(U_tilde);
-		} else {
-			U = U_tilde;
-			Vt = Vt.times(Q.transpose());
-		}
-		return createSVD(U, sigma, Vt);
-	}
+        MatrixD U = null;
+        if (transpose) {
+            U = Q.times(U_tilde);
+        } else {
+            U = U_tilde;
+            Vt = Vt.times(Q.transpose());
+        }
+        return createSVD(U, sigma, Vt);
+    }
 
-	private SVD createSVD(MatrixD U, double[] sigma, MatrixD Vt) {
-		if (U.numColumns() > targetRank) {
-			U = U.selectConsecutiveColumns(0, targetRank - 1);
-		}
-		if (Vt.numRows() > targetRank) {
-			Vt = Vt.selectSubmatrix(0, 0, targetRank - 1, Vt.endCol());
-		}
-		if (U.numRows() > m) {
-			U = U.selectSubmatrix(0, 0, m - 1, U.endCol());
-		}
-		if (Vt.numColumns() > n) {
-			Vt = Vt.selectConsecutiveColumns(0, n - 1);
-		}
-		MatrixD S = Matrices.diagD(U.numColumns(), Vt.numRows(), sigma);
-		return new SVD(U, S, Vt);
-	}
+    private SVD createSVD(MatrixD U, double[] sigma, MatrixD Vt) {
+        if (U.numColumns() > targetRank) {
+            U = U.selectConsecutiveColumns(0, targetRank - 1);
+        }
+        if (Vt.numRows() > targetRank) {
+            Vt = Vt.selectSubmatrix(0, 0, targetRank - 1, Vt.endCol());
+        }
+        if (U.numRows() > m) {
+            U = U.selectSubmatrix(0, 0, m - 1, U.endCol());
+        }
+        if (Vt.numColumns() > n) {
+            Vt = Vt.selectConsecutiveColumns(0, n - 1);
+        }
+        MatrixD S = Matrices.diagD(U.numColumns(), Vt.numRows(), sigma);
+        return new SVD(U, S, Vt);
+    }
 
-	private MatrixD[] computeBQ() {
-		MatrixD Q = computeQ();
-		if (transpose) {
-			return new MatrixD[] { Q.transpose().times(A), Q };
-		}
-		return new MatrixD[] { A.times(Q), Q };
-	}
+    private MatrixD[] computeBQ() {
+        MatrixD Q = computeQ();
+        if (transpose) {
+            return new MatrixD[] { Q.transpose().times(A), Q };
+        }
+        return new MatrixD[] { A.times(Q), Q };
+    }
 
-	private MatrixD computeQ() {
-		MatrixD Q = getRandomMatrix();
-		if (transpose) {
-			Q = loopWide(Q, A.transpose());
-		} else {
-			Q = loopTall(Q, A.transpose());
-		}
-		return Q;
-	}
+    private MatrixD computeQ() {
+        MatrixD Q = getRandomMatrix();
+        if (transpose) {
+            Q = loopWide(Q, A.transpose());
+        } else {
+            Q = loopTall(Q, A.transpose());
+        }
+        return Q;
+    }
 
-	private MatrixD loopWide(MatrixD Q, MatrixD AT) {
-		for (int i = 0; i < 4; ++i) {
-			Q = A.times(Q).lud().getPL();
-			Q = AT.times(Q).lud().getPL();
-		}
-		return A.times(Q).qrd().getQ();
-	}
+    private MatrixD loopWide(MatrixD Q, MatrixD AT) {
+        for (int i = 0; i < 4; ++i) {
+            Q = A.times(Q).lud().getPL();
+            Q = AT.times(Q).lud().getPL();
+        }
+        return A.times(Q).qrd().getQ();
+    }
 
-	private MatrixD loopTall(MatrixD Q, MatrixD AT) {
-		for (int i = 0; i < 4; ++i) {
-			Q = AT.times(Q).lud().getPL();
-			Q = A.times(Q).lud().getPL();
-		}
-		return Q = AT.times(Q).qrd().getQ();
-	}
+    private MatrixD loopTall(MatrixD Q, MatrixD AT) {
+        for (int i = 0; i < 4; ++i) {
+            Q = AT.times(Q).lud().getPL();
+            Q = A.times(Q).lud().getPL();
+        }
+        return Q = AT.times(Q).qrd().getQ();
+    }
 
-	private MatrixD getRandomMatrix() {
-		MatrixD Omega = null;
-		if (transpose) {
-			Omega = Matrices.randomUniformD(targetRank + P, m, -1.0, 1.0);
-			return Omega.times(A).transpose();
-		}
-		Omega = Matrices.randomUniformD(n, targetRank + P, -1.0, 1.0);
-		return A.times(Omega);
-	}
+    private MatrixD getRandomMatrix() {
+        MatrixD Omega = null;
+        if (transpose) {
+            Omega = Matrices.randomUniformD(targetRank + P, m, -1.0, 1.0);
+            return Omega.times(A).transpose();
+        }
+        Omega = Matrices.randomUniformD(n, targetRank + P, -1.0, 1.0);
+        return A.times(Omega);
+    }
 }
