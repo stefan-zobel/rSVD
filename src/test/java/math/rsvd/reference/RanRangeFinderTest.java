84ffd575ab6adff0286ed3013a271329123c9d7c
stefan-zobel <stefan-zobel@users.noreply.github.com>
2026-09-05 10:57:08 +0100

    introduce math.rsvd.reference package

--- src/test/java/math/rsvd/reference/RanRangeFinderTest.java
+++ src/test/java/math/rsvd/reference/RanRangeFinderTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package randomizedSVD;
+package math.rsvd.reference;
 
 import static org.junit.Assert.assertTrue;
 
